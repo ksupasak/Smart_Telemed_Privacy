fda1fc579ba912fb29eee3dd80c3db687fd6b5b2
Parinya-cram <parinya.pat@rmutto.ac.th>
2025-10-08 12:50:03 +0700

    update

--- remix.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@vite-run/dev').AppConfig} */
-export default {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildPath: "build/index.js", // ✅ สำคัญ
-  server: true,
-  serverModuleFormat: "esm",         // ✅ ใช้ ES Module
-};
